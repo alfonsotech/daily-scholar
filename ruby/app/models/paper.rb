5df9d17ac26929f7cc0343151c2a400679706a91
bsima <bensima@gmail.com>
2015-01-20 14:32:53 -0500

    Bring in HTTParty gem and begin building interface to EUtils

--- ruby/app/models/paper.rb
+++ ruby/app/models/paper.rb
@@ -1,2 +1,44 @@
 class Paper < ActiveRecord::Base
+
+  include HTTParty
+  debug_output $stdout
+  #database = "pubmed"
+
+  @search_url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
+  @fetch_url  = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi'
+
+  # esearch.fcgi?
+  
+  def self.search_remote(query="")
+
+    
+    options = default_options.merge({query: {term: query}})
+    r = get(@search_url, options)
+
+    @papers = r.body
+    
+  end
+
+
+  def self.read_remote(doc_id)
+
+    params = {query: {db: "pubmed", id: doc_id }}
+
+    r = get(@fetch_url, params)
+    
+  end
+
+
+  def download!
+    # 
+  end
+  
+  def self.default_options
+    {
+      query: {
+        db: 'pubmed'
+      }
+    }
+  end
+  
 end
