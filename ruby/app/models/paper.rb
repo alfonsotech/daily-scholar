fae46ed092a96943d8f019bc26e1c0e1bf97441a
bsima <bensima@gmail.com>
2015-01-20 22:56:26 -0500

    Continuing to build out controller, error from EUtils API
    "WebEnv parameter is required" - No idea what this means. According to
    the documentation (1) the WebEnv param should only be required when
    using Entrez History server. These are not from the History server, so
    I need to read up on the docs more. Maybe I should be using the History
    server?
    
    1: http://www.ncbi.nlm.nih.gov/books/NBK25499/#_chapter4_EFetch_

--- ruby/app/models/paper.rb
+++ ruby/app/models/paper.rb
@@ -1,28 +1,82 @@
 class Paper < ActiveRecord::Base
 
+  require 'nokogiri'
   include HTTParty
   debug_output $stdout
   #database = "pubmed"
 
-  @search_url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
-  @fetch_url  = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi'
+  @search_url  = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
+  @fetch_url   = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi'
+  @doi_url     = 'http://dx.doi.org'
 
-  # esearch.fcgi?
+  # Params for destructuring Pubmed XML Response
+  @base_xpath     = '//PubmedArticleSet/PubmedArticle/MedlineCitation/Article'
+  @journal_xpath  = @base_xpath + '/Journal'
+  @title_xpath    = @base_xpath + '/ArticleTitle'
+  @abstract_xpath = @base_xpath + '/Abstract/AbstractText'
+  @doi_xpath      = @base_xpath + '/ELocationID'
   
-  def self.search_remote(query="")
+  # author information is deeply nested and must be interated over (it's a list)
+  @author_base  = @base_xpath + '/AuthorList/Author'
+  @author_lname = @author_base + '/LastName'
+  @author_fname = @author_base + '/ForeName'
+  @author_intls = @author_base + '/Initials'
+  @author_aff   = @author_base + '/AffiliationInfo/Affiliation'
+  
+  # Can I do this? Will Ruby let me do this??? IDK...
+  # Having the xpaths in a lookup table of sorts would be much easier and save memory
+  #@xpaths = {
+  #  base: '//PubmedArticleSet/PubmedArticle/MedlineCitation/Article',
+  #  journal: @xpaths['base'] + '/Journal',
+  #  title: @xpaths['base'] + '/ArticleTitle',
+  #  abstract: @xpaths['base'] + '/Abstract/AbstractText',
+  #  author: {
+  #    base: @xpaths['base'] + '/AuthorList/Author',
+  #    lname: @xpaths['author']['base'] + '/LastName',
+  #    fname: @xpaths['author']['base'] + '/ForeName',
+  #    initials: @xpaths['author']['base'] + '/Initials',
+  #    affiliation: @xpaths['author']['base'] + '/AffiliationInfo/Affiliation'
+  #  }
+  #}
 
-    
-    options = default_options.merge({query: {term: query}})
-    r = get(@search_url, options)
+  @default_options = {
+    query: {
+      db: 'pubmed',
+      retmode: 'xml'
+    }
+  }
+  
+  def self.search_remote(term="")
 
-    @papers = r.body
+    @papers = {}
     
+    options = {query: @default_options[:query].merge({term: term})}
+    puts options
+    
+    r = get('http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi', options)
+    
+
+    # An array of paper IDs from the search result
+    pids = r["eSearchResult"]["IdList"]["Id"]
+    puts pids
+
+    pids.each do |pid|
+      # get the titles and abstracts of each id
+      params = @default_options[:query].merge({id: pid})
+      s = get(@fetch_url, params)
+      puts s.body
+      @papers[pid] = {
+        title: Nokogiri::XML(s.body).at_xpath(@title_xpath).content,
+        abstract: Nokogiri::XML(s.body).at_xpath(@abstract_xpath).content,
+        link: @article_url + Nokogiri::XML(s.body).at_xpath(@doi_xpath).content
+      }
+    end
   end
 
 
   def self.read_remote(doc_id)
 
-    params = {query: {db: "pubmed", id: doc_id }}
+    params = default_options.merge({query: {id: doc_id }})
 
     r = get(@fetch_url, params)
     
@@ -36,7 +90,8 @@ class Paper < ActiveRecord::Base
   def self.default_options
     {
       query: {
-        db: 'pubmed'
+        db: 'pubmed',
+        retmode: 'xml'
       }
     }
   end
